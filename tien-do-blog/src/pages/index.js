0548bee10d00fbbeb8882370e7bd1d15c2cc5804
Mac M1 <tienthanhapp278@gmail.com>
2021-06-15 16:48:16 +0700

    Add new

---
+++ tien-do-blog/src/pages/index.js
@@ -0,0 +1,16 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const IndexrPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default IndexrPage
---
+++ tien-do-blog/src/pages/test.js
@@ -0,0 +1,16 @@
+// Step 1: Import your component
+import * as React from 'react'
+
+// Step 2: Define your component
+const TestPage = () => {
+  return (
+    <main>
+      <title>Test Me</title>
+      <h1>About Me</h1>
+      <p>Hi there! This is another page created by Gatsby! WOWWWWWW.</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default TestPage
